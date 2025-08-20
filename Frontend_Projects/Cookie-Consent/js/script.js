8e1d01e8f84b7c63b8dc6415c16c19004327fa5e
DevMasen <mohammadwh400@gmail.com>
2025-08-20 15:28:23 +0330

    feat: Cookie Consent project is full featured

--- Frontend_Projects/Cookie-Consent/js/script.js
+++ Frontend_Projects/Cookie-Consent/js/script.js
@@ -1,1 +1,36 @@
 'use strict';
+const rejectBtn = document.querySelector('.reject-cookie-btn');
+const acceptBtn = document.querySelector('.accept-cookie-btn');
+const cookieModal = document.querySelector('.cookie-consent');
+
+function setCookieConsent(expireDays = 1, isAccepted = false) {
+	const now = new Date().getTime();
+	const expireTime = now + expireDays * 24 * 60 * 60 * 1000;
+	// 1 min = 0.0006944444 day
+	const consentObj = {
+		value: isAccepted,
+		expire: expireTime,
+	};
+	localStorage.setItem('cookie-consent', JSON.stringify(consentObj));
+}
+window.addEventListener('load', () => {
+	const cookieConsent = localStorage.getItem('cookie-consent');
+	const consentObj = JSON.parse(cookieConsent);
+	if (!cookieConsent || consentObj?.expire < new Date().getTime()) {
+		cookieModal.style.display = 'flex';
+		localStorage.removeItem('cookie-consent');
+	}
+	// console.log(consentObj);
+});
+
+rejectBtn.addEventListener('click', () => {
+	setCookieConsent(0.000694444, false);
+	// 1 minutes expiration
+	cookieModal.style.display = 'none';
+});
+
+acceptBtn.addEventListener('click', () => {
+	setCookieConsent(0.000694444, true);
+	// 1 minutes expiration
+	cookieModal.style.display = 'none';
+});
