590bd67763443e239586064164fadb4defa67005
DevMasen <mohammadwh400@gmail.com>
2025-08-21 16:58:26 +0330

    feat: Restricted Textarea project if full featured

--- Frontend_Projects/Restricted-Textarea/js/script.js
+++ Frontend_Projects/Restricted-Textarea/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+const textareaContainer = document.querySelector('.textarea-container');
+const messageInput = document.getElementById('message');
+const typedLetters = document.getElementById('typed-letters');
+const limit = document.getElementById('limit');
+
+const maxLength = +messageInput.getAttribute('maxlength');
+let typedLength = 0;
+typedLetters.textContent = typedLength;
+limit.textContent = maxLength;
+
+messageInput.addEventListener('input', e => {
+	typedLength = e.target.value.length;
+	typedLetters.textContent = typedLength;
+	textareaContainer.classList.remove('reached-limit');
+	if (typedLength === maxLength)
+		textareaContainer.classList.add('reached-limit');
+});
