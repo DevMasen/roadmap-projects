ea1d2755669d33c0aceab477f445dd8b68495170
DevMasen <mohammadwh400@gmail.com>
2025-08-21 21:45:34 +0330

    feat: Accordion project is full featured

--- Frontend_Projects/Accordion/js/script.js
+++ Frontend_Projects/Accordion/js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+const tabList = document.querySelector('.tab-list');
+const tabItems = document.querySelectorAll('.tab-item');
+
+function removeByClassName(nodeList = [], className = '') {
+	for (const element of nodeList) {
+		element.classList.remove(className);
+	}
+}
+
+tabList.addEventListener('click', e => {
+	const selectedTab = e.target.closest('.tab-item');
+	console.log(selectedTab);
+	if (selectedTab.classList.contains('open')) {
+		selectedTab.classList.remove('open');
+		return;
+	}
+	removeByClassName(tabItems, 'open');
+	selectedTab.classList.add('open');
+});
