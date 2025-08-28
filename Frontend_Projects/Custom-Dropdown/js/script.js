240ba1ea9ddcdb4305529c08beb3e51ee3809539
DevMasen <mohammadwh400@gmail.com>
2025-08-28 21:27:22 +0330

    feat: Custom Dropdown if full featured

--- Frontend_Projects/Custom-Dropdown/js/script.js
+++ Frontend_Projects/Custom-Dropdown/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+const dropdownSelected = document.querySelector('.dropdown-selected');
+const selectedText = document.querySelector('.selected-text');
+const dropdownContainer = document.querySelector('.dropdown-container');
+const dropdownSelect = document.querySelector('.dropdown-select');
+const options = document.querySelectorAll('.dropdown-option');
+dropdownSelected.addEventListener('click', () => {
+	dropdownContainer.classList.toggle('open');
+});
+dropdownSelect.addEventListener('click', e => {
+	const selectedItem = e.target.closest('.dropdown-option');
+	[...options].map(option => {
+		option.classList.remove('selected');
+	});
+	selectedItem.classList.add('selected');
+	selectedText.textContent =
+		selectedItem.querySelector('.option-text').textContent;
+	dropdownContainer.classList.remove('open');
+});
