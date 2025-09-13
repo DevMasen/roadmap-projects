ba22bc2981147d6ba9efd1be1ffb26a0389e7906
DevMasen <mohammadwh400@gmail.com>
2025-09-13 11:38:33 +0330

    feat: html structure implemented

--- Frontend_Projects/GitHub-Random-Repository/js/script.js
+++ Frontend_Projects/GitHub-Random-Repository/js/script.js
@@ -1,2 +1,22 @@
 'use strict';
 import langData from './data.js';
+
+//! Dropdown interaction
+const dropdownSelected = document.querySelector('.dropdown-selected');
+const selectedText = document.querySelector('.selected-text');
+const dropdownContainer = document.querySelector('.dropdown-container');
+const dropdownSelect = document.querySelector('.dropdown-select');
+const options = document.querySelectorAll('.dropdown-option');
+dropdownSelected.addEventListener('click', () => {
+	dropdownContainer.classList.toggle('open');
+});
+dropdownSelect.addEventListener('click', e => {
+	const selectedItem = e.target.closest('.dropdown-option');
+	[...options].map(option => {
+		option.classList.remove('selected');
+	});
+	selectedItem.classList.add('selected');
+	selectedText.textContent =
+		selectedItem.querySelector('.option-text').textContent;
+	dropdownContainer.classList.remove('open');
+});
