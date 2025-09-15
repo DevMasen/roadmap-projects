7f5ee46de42b91ace0005bd862863a2aed3da4e0
DevMasen <mohammadwh400@gmail.com>
2025-09-15 14:52:53 +0330

    feat: GitHub Random Repository is full featured

--- Frontend_Projects/GitHub-Random-Repository/js/script.js
+++ Frontend_Projects/GitHub-Random-Repository/js/script.js
@@ -21,8 +21,6 @@ const issuesCount = document.querySelector('.issues-count');
 
 // Global Variables
 const numberFormat = new Intl.NumberFormat('en-US');
-const randomIndex = Math.floor(Math.random() * 10);
-const randomIndexForAllLanguages = Math.floor(Math.random() * 100);
 
 // Functions
 function initLangData() {
@@ -36,18 +34,42 @@ function initLangData() {
 	});
 }
 
-async function getSelectedLangData(e) {
-	const selectedItem = e.target.closest('.dropdown-option');
-	const selectedLanguage =
-		selectedItem.querySelector('.option-text').textContent;
+function updateRepoResultUI(langItem) {
+	stats.classList.remove('loading');
+	stats.classList.add('wait');
+	stats.style.display = 'none';
+	repoResult.classList.remove('hidden');
+	repoName.textContent = langItem.name;
+	repoDescription.textContent = langItem.description;
+	repoLink.setAttribute('href', langItem.svn_url);
+	langName.textContent = langItem.language;
+	starsCount.textContent = numberFormat.format(langItem.stargazers_count);
+	forksCount.textContent = numberFormat.format(langItem.forks_count);
+	issuesCount.textContent = numberFormat.format(langItem.open_issues_count);
+	refreshBtn.style.display = 'block';
+}
+function updateLoadingUI() {
+	stats.style.display = 'flex';
+	repoResult.classList.add('hidden');
+	dropdownContainer.classList.remove('open');
+	refreshBtn.style.display = 'none';
+	stats.classList.remove('error');
+	stats.classList.remove('wait');
+	stats.classList.add('loading');
+}
 
-	// update ui
-	selectedText.textContent = selectedLanguage;
+function updateErrorUI(e) {
+	statsError.textContent = e.message;
+	stats.classList.remove('wait');
+	stats.classList.remove('loading');
+	stats.classList.add('error');
+	retryBtn.style.display = 'block';
+}
 
-	// Fetching repo data
+async function fetchLanguage(lang = '') {
 	try {
 		const response = await fetch(
-			`https://api.github.com/search/repositories?q=language:${selectedLanguage.toLowerCase()}ppp&per_page=100`,
+			`https://api.github.com/search/repositories?q=language:${lang.toLowerCase()}&per_page=100`,
 			{
 				method: 'GET',
 				headers: {
@@ -61,39 +83,39 @@ async function getSelectedLangData(e) {
 				`Error fetching repository!(code ${response.status})`
 			);
 		const data = await response.json();
+		const randomIndex = Math.floor(Math.random() * 10);
+		const randomIndexForAllLanguages = Math.floor(Math.random() * 100);
 		const index =
-			selectedLanguage.toLowerCase() === 'all languages'
+			lang.toLowerCase() === 'all languages'
 				? randomIndexForAllLanguages
 				: randomIndex;
 		const langItem = data.items[index];
 
 		// updating ui
-		stats.classList.remove('loading');
-		stats.classList.add('wait');
-		stats.style.display = 'none';
-		repoResult.classList.remove('hidden');
-		repoName.textContent = langItem.name;
-		repoDescription.textContent = langItem.description;
-		repoLink.setAttribute('href', langItem.svn_url);
-		langName.textContent = langItem.language;
-		starsCount.textContent = numberFormat.format(langItem.stargazers_count);
-		forksCount.textContent = numberFormat.format(langItem.forks_count);
-		issuesCount.textContent = numberFormat.format(
-			langItem.open_issues_count
-		);
-		refreshBtn.style.display = 'block';
+		updateRepoResultUI(langItem);
 	} catch (e) {
-		// update ui
-		statsError.textContent = e.message;
-		stats.classList.remove('wait');
-		stats.classList.remove('loading');
-		stats.classList.add('error');
-		retryBtn.style.display = 'block';
+		// error status
+		updateErrorUI(e);
 	}
 }
 
-function handleRefresh() {}
-function handleRetry() {}
+async function getSelectedLangData(e) {
+	const selectedItem = e.target.closest('.dropdown-option');
+	const selectedLanguage =
+		selectedItem.querySelector('.option-text').textContent;
+
+	// update ui
+	selectedText.textContent = selectedLanguage;
+
+	// Fetching repo data
+	fetchLanguage(selectedLanguage);
+}
+
+function refresh() {
+	const selectedLanguage = selectedText.textContent;
+	fetchLanguage(selectedLanguage);
+	updateLoadingUI();
+}
 
 // Initialization
 initLangData();
@@ -103,16 +125,14 @@ dropdownSelected.addEventListener('click', () => {
 	dropdownContainer.classList.toggle('open');
 });
 dropdownSelect.addEventListener('click', e => {
+	// get clicked language data
 	getSelectedLangData(e);
 
-	// update ui
-	stats.style.display = 'flex';
-	repoResult.classList.add('hidden');
-	dropdownContainer.classList.remove('open');
-	refreshBtn.style.display = 'none';
-	stats.classList.remove('error');
-	stats.classList.remove('wait');
-	stats.classList.add('loading');
+	// loading status
+	updateLoadingUI();
 });
-refreshBtn.addEventListener('click', handleRefresh);
-retryBtn.addEventListener('click', handleRetry);
+refreshBtn.addEventListener('click', refresh);
+retryBtn.addEventListener('click', () => {
+	refresh();
+	retryBtn.style.display = 'none';
+});
